d80faa5917a205106b015c0b19551ae4f7732695
LucasGoncalo <lucasgoncalodossantos2018@gmail.com>
2024-05-25 15:26:36 -0300

    Nova atualização do código

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 //efeito do botão voltar ao Topo
-
+function topo() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
 //Validação de Login
+function login() {
+  var logado = 0;
+  var usuario = document.getElementById("usuario").value;
+  var senha = document.getElementById("senha").value;
 
+  if (usuario == "lucas" && senha == "123456789") {
+    window.location = "index.html";
+    logado = 1;
+  } else logado == 0;
+  {
+    alert("Dados errados");
+  }
+}
 //Ativar alert no botão cadastrar
